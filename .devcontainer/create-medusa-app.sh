f84d7e1d41131a2f6669ccfe9c2a62fb93da7537
Y0n1x <fr.may.johann@gmail.com>
2024-10-30 03:49:50 +0100

    feat(Global): 🚧 add latest yarn with install script, maybe fix devcontainer issues

--- .devcontainer/create-medusa-app.sh
+++ .devcontainer/create-medusa-app.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# Update yarn to the latest version
-# npm install -g yarn
-
-# Install Medusa CLI globally using Yarn
-# yarn global add @medusajs/medusa-cli
-
-# Create a new Nx workspace for Medusa apps using Yarn and disable interactive mode
-# nx init
-
 # Create a new Medusa app using Medusa CLI with the specified name and starter URL, and disable interactive mode
 # medusa new driptimez https://github.com/tinloof/medusa-b2c-starter -y --v2
-npx create-medusa-app@latest --repo-url https://github.com/tinloof/medusa-b2c-starter --with-nextjs-starter --v2 --verbose
+
+yarn create medusa-app@latest --repo-url https://github.com/tinloof/medusa-b2c-starter --verbose
 
 # Install dependencies and initialize the Medusa app using Yarn
 # cd driptimez || exit
