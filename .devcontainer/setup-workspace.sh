0a2a5bcba0c1e4b1fa16521a561a73c55f91560e
Y0n1x <fr.may.johann@gmail.com>
2024-10-31 21:43:15 +0100

    fix(Dev): :ambulance:

--- .devcontainer/setup-workspace.sh
+++ .devcontainer/setup-workspace.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Update yarn to the latest version
-yarn set version canary && yarn install
+cd .. | yarn set version canary && yarn install
 
 # Install Medusa CLI globally using Yarn
-# yarn global add @medusajs/medusa-cli
-
-# Create a new Nx workspace for Medusa apps using Yarn and disable interactive mode
-# nx init
+cd .. | yarn global add @medusajs/medusa-cli
