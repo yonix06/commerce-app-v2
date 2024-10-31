4abf87a5dc0cb92830905b373b45c1de64e321dc
Y0n1x <fr.may.johann@gmail.com>
2024-10-31 23:44:06 +0100

    refactor(Docker Images): :art:

--- .devcontainer/build-push-devcontainer.sh
+++ .devcontainer/build-push-devcontainer.sh
@@ -1,6 +1,10 @@
 #! /bin/sh
 
-# Build the image and push to Docker Hub
+# Build the images and push to Docker Hub
 docker buildx build -f Dockerfile.ubernode -t yonix06/dev:node23-yarn4 --push .
+
+echo "Node23-yarn4 image built and pushed to Docker Hub"
+
 docker buildx build -f Dockerfile.dev -t yonix06/dev:devcontainer-db --push .
 
+echo "Devcontainer-db image built and pushed to Docker Hub"
