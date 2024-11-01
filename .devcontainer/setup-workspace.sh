f8010a6b8c3cf4cbb09ec7cccee0608b54e0120d
Y0n1x <fr.may.johann@gmail.com>
2024-11-01 00:48:37 +0000

    refactor(Global): :art: Reorder

--- .devcontainer/setup-workspace.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Update yarn to the latest version
-cd .. | yarn set version canary && yarn install
-
-# Install Medusa CLI globally using Yarn
-cd .. | yarn global add @medusajs/medusa-cli
