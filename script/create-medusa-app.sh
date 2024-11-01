885129b7efde858877cf98cc5ed95e86433e8502
Y0n1x <fr.may.johann@gmail.com>
2024-11-01 00:52:11 +0000

    fix(Dev): :art:

--- script/create-medusa-app.sh
+++ script/create-medusa-app.sh
@@ -3,7 +3,7 @@
 # Create a new Medusa app using Medusa CLI with the specified name and starter URL, and disable interactive mode
 # medusa new driptimez https://github.com/tinloof/medusa-b2c-starter -y --v2
 
-cd .. && yarn create medusa-app@latest --directory-path ./apps --verbose
+cd .. | yarn create medusa-app@latest --directory-path ./apps --verbose
 
 # Install dependencies and initialize the Medusa app using Yarn
 # cd store || exit
