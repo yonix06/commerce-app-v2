a9477e1f69985c9ceb9c47ad7751c210ae3f16c5
Y0n1x <fr.may.johann@gmail.com>
2024-11-01 09:41:36 +0000

    fix(Dev): :bug:

--- script/create-medusa-app.sh
+++ script/create-medusa-app.sh
@@ -3,6 +3,7 @@
 # Create a new Medusa app using Medusa CLI with the specified name and starter URL, and disable interactive mode
 # medusa new driptimez https://github.com/tinloof/medusa-b2c-starter -y --v2
 
+cd .. | yarn add -D @medusajs/telemetry fs-exists-cached
 cd .. | yarn create medusa-app@latest --directory-path ./apps --verbose --db-url postgres://postgres:password@store-database:5432
 
 # Install dependencies and initialize the Medusa app using Yarn
