a93aa92148896d6bf852a293f12ce9c8ea5ec8b2
Y0n1x <fr.may.johann@gmail.com>
2024-11-01 07:18:38 +0000

    fix(Workspace): :zap:

--- .devcontainer/build-push-devcontainer.sh
+++ .devcontainer/build-push-devcontainer.sh
@@ -5,6 +5,6 @@ docker buildx build -f Dockerfile.ubernode -t yonix06/dev:node23-yarn4 --push .
 
 echo "Node23-yarn4 image built and pushed to Docker Hub"
 
-docker buildx build -f Dockerfile.dev -t yonix06/dev:devcontainer-db --push .
+docker buildx build -f Dockerfile.dev -t yonix06/dev:devcontainer --push .
 
 echo "Devcontainer-db image built and pushed to Docker Hub"
